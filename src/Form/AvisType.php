86c7bae093102c42b0c4ebeba056b05e21798b79
Laura <lam.laura65@gmail.com>
2021-07-28 17:43:55 +0200

    Version prefinale

--- src/Form/AvisType.php
+++ src/Form/AvisType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Avis;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -16,8 +18,34 @@ class AvisType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('object', TextType::class)
-            ->add('content', TextareaType::class)
+            ->add('object', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez écrire un objet ',
+                    ]),
+                ],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'style' => 'height: 150px',
+                    'placeholder' => 'Ecrire ici votre message',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez ne pas envoyer un message vide',
+                    ]),
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'Veuillez écrire pour ne pas envoyer de message vide',
+                        'max' => 200,
+                        'maxMessage' => 'Le message ne doit pas dépasser 200 caractères',
+                        // max length allowed by Symfony for security reasons
+                        // 'max' => 4096,
+                    ]),
+                ],
+            ])
 
             ->add('envoyer', SubmitType::class);
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,26 +4,41 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
+            ->add('email', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez écrire votre email ',
+                    ]),
+                ],
+            ])
+            ->add('username', TextType::class, [
+                'label' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez écrire votre nom d\'utilisateur ',
+                    ]),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Veuillez accepter les conditions.',
                     ]),
                 ],
             ])
@@ -31,20 +46,20 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit avoir au minimum {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
